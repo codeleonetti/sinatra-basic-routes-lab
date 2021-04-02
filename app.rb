060812aa5c2e50ee943a0ff1e35708af8936a785
john <john.d.leonetti@gmail.com>
2021-04-02 12:55:14 -0400

    commit tree f0ced241981dca40c82bb776e11a281fb017ab66

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do 
+        "My name is John"
+    end
+
+    get '/hometown' do
+        "My hometown is Spokane, WA"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is I don't know"
+    end
 end
